7aeedb4059422b10a902651c209a4c24cd09fac8
Ayan Pal <ayan.pal@live.com>
2021-07-04 21:27:40 +0530

    seperated store library code and added constants for action types

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
+// State management library code
+const createStore = (reducer) => {
+  // The store should have 4 parts
+  // 4. Update state
+  
+  // 1. The state  
+  let state;
+    let listners = [];
+  
+    // 2. Get the state
+    const getState = () => state;
+  
+    // 3. Listen to change in state
+    const subscribe = (listner) => {
+      listners.push(listner);
+      // returning an unsubscribe method which will remove the callback function from the listners array
+      return () => {
+        listners = listners.filter((l) => l !== listner );
+      };
+    };
+  
+    // 4. Update the state
+    const dispatch = (action) => {
+      // updating the state
+      state = reducer(state, action);
+  
+      // invoking all listners function
+      listners.forEach((listner) => listner());
+    };
+  
+    return {
+      getState,
+      subscribe,
+      dispatch,
+    };
+  };
+
+// App code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 // todos reducer function
 const todos = (state = [], action) => {
 
   switch(action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map((todo) => todo.id !== action.id ? todo : {
         ...todo,
         complete: !todo.complete
@@ -19,9 +64,9 @@ const todos = (state = [], action) => {
 // goals reducer function
 const goals = (state = [], action) => {
   switch(action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id);
     default:
       return state;
@@ -35,41 +80,3 @@ const app = (state = {}, action) => {
     goal: goals(state.goal, action),
   }
 };
-
-const createStore = (reducer) => {
-// The store should have 4 parts
-// 4. Update state
-
-// 1. The state  
-let state;
-  let listners = [];
-
-  // 2. Get the state
-  const getState = () => state;
-
-  // 3. Listen to change in state
-  const subscribe = (listner) => {
-    listners.push(listner);
-    // returning an unsubscribe method which will remove the callback function from the listners array
-    return () => {
-      listners = listners.filter((l) => l !== listner );
-    };
-  };
-
-  // 4. Update the state
-  const dispatch = (action) => {
-    // updating the state
-    state = reducer(state, action);
-
-    // invoking all listners function
-    listners.forEach((listner) => listner());
-  };
-
-  return {
-    getState,
-    subscribe,
-    dispatch,
-  };
-};
-
-const store = createStore(app)
